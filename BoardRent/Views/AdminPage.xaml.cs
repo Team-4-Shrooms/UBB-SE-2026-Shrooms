c9bf44b19526114fddb869ed18a57057496b478f
ioan-one <ioan.alexandru.tudor@stud.ubbcluj.ro>
2026-03-25 17:56:24 +0200

    Implement Admin Service, ViewModel, and View

--- BoardRent/Views/AdminPage.xaml.cs
+++ BoardRent/Views/AdminPage.xaml.cs
@@ -1,14 +1,30 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using BoardRent.Services;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+using System.ComponentModel;
 using BoardRent.ViewModels;
 using BoardRent.Utils;
-using BoardRent.Repositories;
-using BoardRent.Data;
-using System.ComponentModel;
 
 namespace BoardRent.Views
 {
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
     public sealed partial class AdminPage : Page, INotifyPropertyChanged
     {
         public AdminViewModel ViewModel { get; }
@@ -16,23 +32,14 @@ namespace BoardRent.Views
         public AdminPage()
         {
             InitializeComponent();
-            
-            // Temporary manual DI wire-up (repositories currently blocked/wip)
-            // Replace with proper Dependency Injection as project evolves.
-            var dbContext = new AppDbContext();
-            var userRepository = new UserRepository(dbContext);
-            var failedLoginRepository = new FailedLoginRepository(dbContext);
-            var adminService = new AdminService(userRepository, failedLoginRepository);
-            
-            ViewModel = new AdminViewModel(adminService);
+            // TODO: Resolve proper DI once unblocked
+            ViewModel = new AdminViewModel(null);
             ViewModel.PropertyChanged += ViewModel_PropertyChanged;
         }
 
         public bool IsUnauthorized => !SessionContext.GetInstance().IsLoggedIn || SessionContext.GetInstance().Role != "Administrator";
-        
         public Visibility IsAuthorizedVisibility => IsUnauthorized ? Visibility.Collapsed : Visibility.Visible;
-
-        public bool IsErrorVisible => !string.IsNullOrEmpty(ViewModel.ErrorMessage);
+        public bool IsErrorVisible => ViewModel != null && !string.IsNullOrEmpty(ViewModel.ErrorMessage);
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -47,7 +54,7 @@ namespace BoardRent.Views
         private void OnSignOutClicked(object sender, RoutedEventArgs e)
         {
             SessionContext.GetInstance().Clear();
-            App.NavigateTo(typeof(LoginPage));
+            // App.NavigateTo(typeof(LoginPage));
         }
     }
 }
